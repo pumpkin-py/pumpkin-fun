98ff3f421102f4c8225ec5b2d3316c66d890de06
ArcasCZ <VydraMan@seznam.cz>
2021-10-28 16:59:33 +0200

    DHash channel management

---
+++ dhash/database.py
@@ -0,0 +1,93 @@
+from __future__ import annotations
+
+from typing import Tuple
+
+from sqlalchemy import BigInteger, Column, Integer, String
+
+from database import database, session
+
+class Hash(database.base):
+    """Stored image hashes"""
+
+    __tablename__ = "fun_dhash_hashes"
+
+    idx = Column(Integer, primary_key=True, autoincrement=True)
+    guild_id = Column(BigInteger)
+    channel_id = Column(BigInteger)
+    message_id = Column(BigInteger)
+    attachment_id = Column(BigInteger)
+    hash = Column(String)
+    
+    __table_args__ = (
+        UniqueConstraint(guild_id, channel_id),
+        UniqueConstraint(message_id, attachment_id),
+    )
+
+    
+
+    def __repr__(self) -> str:
+        return (
+            f'<{self.__class__.__name} idx="{self.idx}" guild_id="{self.guild_id}" '
+            f'channel_id="{self.channel_id}" message_id="{self.message_id}" '
+            f'attachement_id="{self.attachment_id}" hash="{self.hash}">'
+        )
+
+    def dump(self) -> dict:
+        return {
+            "guild_id": self.guild_id,
+            "channel_id": self.channel_id,
+            "message_id": self.message_id,
+            "attachment_id": self.attachment_id,
+            "hash": self.hash,
+        }
+
+class HashChannel(database.base):
+    __tablename__ = "fun_dhash_channels"
+
+    idx = Column(Integer, primary_key=True, autoincrement=True)
+    guild_id = Column(BigInteger)
+    channel_id = Column(BigInteger)
+
+    __table_args__ = (
+        UniqueConstraint(guild_id, channel_id),
+    )
+
+    def add(guild_id: int, channel_id: int) -> HashChannel:
+        channel = HashChannel(guild_id=guild_id, channel_id=channel_id)
+        session.add(channel)
+        session.commit()
+        return channel
+
+    def get(guild_id: int, channel_id: int) -> Optional[HashChannel]:
+        query = (
+            session.query(HashChannel)
+            .filter_by(guild_id=guild_id, channel_id=channel_id)
+            .one_or_none()
+        )
+        return query
+
+    def get_all(guild_id: int) -> List[HashChannel]:
+        query = session.query(HashChannel).filter_by(guild_id=guild_id).all()
+        return query
+
+
+    def remove(guild_id: int, channel_id):
+        query = (
+            session.query(HashChannel)
+            .filter_by(guild_id=guild_id, channel_id=channel_id)
+            .delete()
+        )
+        session.commit()
+        return query
+
+    def __repr__(self) -> str:
+        return (
+            f'<{self.__class__.__name__} idx="{self.idx}" '
+            f'guild_id="{self.guild_id}" channel_id="{self.channel_id}">'
+        )
+
+    def dump(self) -> Dict[str, Union[int, str]]:
+        return {
+            "guild_id": self.guild_id,
+            "channel_id": self.channel_id,
+        }
---
+++ dhash/module.py
@@ -0,0 +1,84 @@
+import aiohttp
+import random
+import re
+from typing import Optional, List, Dict
+
+import discord
+from discord.ext import commands
+
+from core import utils, i18n
+
+_ = i18n.Translator("modules/fun").translate
+
+
+class Dhash(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.guild_only()
+    @commands.check(check.acl)
+    @commands.group(name="dhash")
+    async def dhash(self, ctx):
+        await utils.Discord.send_help(ctx)
+
+    @commands.check(check.acl)
+    @dhash.command(name="add")
+    async def dhash_add(self, ctx, channel: discord.TextChannel):
+        hash_channel = HashChannel.get(ctx.guild.id, channel.id)
+        if hash_channel:
+            await ctx.send(
+                _(
+                    ctx,
+                    "{channel} is already hash channel.",
+                ).format(channel=channel.mention)
+            )
+            return
+
+        hash_channel = HashChannel.add(ctx.guild.id, channel.id)
+        await ctx.send(
+            _(
+                ctx,
+                "Channel {channel} added as hash channel.",
+            ).format(channel=channel.mention)
+        )
+        await guild_log.info(
+            ctx.author,
+            ctx.channel,
+            f"Channel #{channel.name} set as hash channel.",
+        )
+
+    @commands.check(check.acl)
+    @dhash.command(name="list")
+    async def dhash_list(self, ctx):
+        hash_channels = HashChannel.get_all(ctx.guild.id)
+        if not hash_channels:
+            await ctx.reply(_(ctx, "This server has no hash channels."))
+            return
+
+        channels = [ctx.guild.get_channel(c.channel_id) for c in hash_channels]
+        column_name_width: int = max([len(c.name) for c in channels if c])
+
+        result = []
+        for hash_channel, channel in zip(hash_channels, channels):
+            name = getattr(channel, "name", "???")
+            line = f"#{name:<{column_name_width}} {hash_channel.channel_id}"
+            result.append(line)
+
+        await ctx.reply("```" + "\n".join(result) + "```")
+
+    @commands.check(check.acl)
+    @dhash.command(name="remove", aliases=["rem"])
+    async def dhash_remove(self, ctx, channel: discord.TextChannel):
+        if HashChannel.remove(ctx.guild.id, channel.id):
+            message = _(ctx, "Hash channel {channel} removed.")
+        else:
+            message = _(ctx, "{channel} is not hash channel.")
+        await ctx.reply(message.format(channel=channel.mention))
+        await guild_log.info(
+            ctx.author,
+            ctx.channel,
+            f"Channel #{channel.name} is no longer a hash channel.",
+        )
+
+def setup(bot) -> None:
+    bot.add_cog(Rand(bot))
