af24ff6613e9e22e34c924bfe459a89e78e13b16
ArcasCZ <VydraMan@seznam.cz>
2021-11-02 11:03:31 +0100

    Typo fix

--- dhash/module.py
+++ dhash/module.py
@@ -335,7 +335,7 @@ class Dhash(commands.Cog):
                         message.embeds[0].footer.text.split(" | ")[1]
                     )
 
-                    self.embed_cache.pop(repost_massage_id)
+                    self.embed_cache.pop(repost_message_id)
 
                     repost_message = await message.channel.fetch_message(
                         repost_message_id
