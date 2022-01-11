484a59f07600f7a0334f55e6127079fa376abee3
charashenko <charashenko@gmail.com>
2022-01-12 00:31:40 +0100

    added json data parser, basic data showcase, weather map

--- weather/module.py
+++ weather/module.py
@@ -19,28 +19,119 @@ class Weather(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @staticmethod
-    def _create_embeds(ctx, url):
-        # create embeds
+    def _get_useful_data(self, json):
+        """
+        example json: https://wttr.in/praha?lang=sk&format=j1
+        get useful data from json as list of individual days,
+        day is a dict with this structure:
+            "date" : str
+            "morning" : dict
+                "state" (clear, sunny...) : str
+                "temp" (in degrees) : str
+                "feels_like" (in degrees) : str
+                "wind_speed" (km/s) : str
+            "day" : dict
+                "state" (clear, sunny...) : str
+                "temp" (in degrees) : str
+                "feels_like" (in degrees) : str
+                "wind_speed" (km/s) : str
+            "evening" : dict
+                "state" (clear, sunny...) : str
+                "temp" (in degrees) : str
+                "feels_like" (in degrees) : str
+                "wind_speed" (km/s) : str
+            "night" : dict
+                "state" (clear, sunny...) : str
+                "temp" (in degrees) : str
+                "feels_like" (in degrees) : str
+                "wind_speed" (km/s) : str
+        """
+
+        weather = []
+        # get individual days to extract data
+        num_of_days = 2 # number of days to get forecast for (including current day, max is 3)
+        for i in range(num_of_days):
+            cur_day = json['weather'][i]
+            weather.append({
+                "date": cur_day['date'],
+                "morning": {
+                    "state": cur_day['hourly'][2]['weatherDesc'][0]['value'],
+                    "temp": cur_day['hourly'][2]['tempC'],
+                    "feels_like": cur_day['hourly'][2]['FeelsLikeC'],
+                    "wind_speed": cur_day['hourly'][2]['windspeedKmph']
+                },
+                "mid_day": {
+                    "state": cur_day['hourly'][4]['weatherDesc'][0]['value'],
+                    "temp": cur_day['hourly'][4]['tempC'],
+                    "feels_like": cur_day['hourly'][4]['FeelsLikeC'],
+                    "wind_speed": cur_day['hourly'][4]['windspeedKmph']
+                },
+                "evening": {
+                    "state": cur_day['hourly'][6]['weatherDesc'][0]['value'],
+                    "temp": cur_day['hourly'][6]['tempC'],
+                    "feels_like": cur_day['hourly'][6]['FeelsLikeC'],
+                    "wind_speed": cur_day['hourly'][6]['windspeedKmph']
+                },
+                "night": {
+                    "state": cur_day['hourly'][7]['weatherDesc'][0]['value'],
+                    "temp": cur_day['hourly'][7]['tempC'],
+                    "feels_like": cur_day['hourly'][7]['FeelsLikeC'],
+                    "wind_speed": cur_day['hourly'][7]['windspeedKmph']
+                }
+            })
+        return weather
+
+    def _create_embeds(self, ctx, name):
+        """create embeds for scrollable embed"""
+        url = f"https://wttr.in/{name}?format=j1"
         request = requests.get(url)
+        # check status code of request for failures (lazy way)
         if request.status_code != 200:
+            # return error embed
             return utils.discord.create_embed(
                 author=ctx.message.author,
-                title="Error occured while getting weather info.",
+                title='Error occured while getting weather info.',
                 error=True,
             )
-        data = request.json()
-        em1 = utils.discord.create_embed(
-            author=ctx.message.author,
-            title="Current weather",
-        )
-        em1.add_field(name="Feels like:", value=data['current_condition'][0]['FeelsLikeC'], inline=True)
-        em2 = utils.discord.create_embed(
-            author=ctx.message.author,
-            title="Current weather",
-        )
-        em2.add_field(name="Feels like:", value=data['current_condition'][0]['temp_C'], inline=True)
-        return [em1, em2]
+        # create day embeds
+        days = self._get_useful_data(request.json())
+        embeds = []
+        for day in days:
+            embed = utils.discord.create_embed(
+                author=ctx.message.author,
+                title=day['date'],
+            )
+            embed.add_field(name='Morning', value=day['morning']['state'], inline=False)
+            embed.add_field(name='Temperature', value=day['morning']['temp'] + ' ˚C', inline=True)
+            embed.add_field(name='Feels like', value=day['morning']['feels_like'] + ' ˚C', inline=True)
+            embed.add_field(name='Wind speed', value=day['morning']['wind_speed'] + ' km/s', inline=False)
+
+            embed.add_field(name='Mid day', value=day['mid_day']['state'], inline=False)
+            embed.add_field(name='Temperature', value=day['mid_day']['temp'] + ' ˚C', inline=True)
+            embed.add_field(name='Feels like', value=day['mid_day']['feels_like'] + ' ˚C', inline=True)
+            embed.add_field(name='Wind speed', value=day['mid_day']['wind_speed'] + ' km/s', inline=True)
+
+            embed.add_field(name='Evening', value=day['evening']['state'], inline=False)
+            embed.add_field(name='Temperature', value=day['evening']['temp'] + ' ˚C', inline=True)
+            embed.add_field(name='Feels like', value=day['evening']['feels_like'] + ' ˚C', inline=True)
+            embed.add_field(name='Wind speed', value=day['evening']['wind_speed'] + ' km/s', inline=True)
+
+            embed.add_field(name='Night', value=day['night']['state'], inline=False)
+            embed.add_field(name='Temperature', value=day['night']['temp'] + ' ˚C', inline=True)
+            embed.add_field(name='Feels like', value=day['night']['feels_like'] + ' ˚C', inline=True)
+            embed.add_field(name='Wind speed', value=day['night']['wind_speed'] + ' km/s', inline=True)
+            embeds.append(embed)
+        
+        # get last "map" emebed
+        embed = utils.discord.create_embed(
+                author=ctx.message.author,
+                title="Weather map",
+            )
+        img_url = f"https://v3.wttr.in/{name}.png"
+        embed.set_image(url=img_url)
+        embeds.append(embed)
+
+        return embeds
 
     @commands.guild_only()
     @commands.check(check.acl)
@@ -122,9 +213,7 @@ class Weather(commands.Cog):
             await ctx.reply(_(ctx, "You have to specify a place or set a preference."))
             return
 
-        url = f"https://wttr.in/{name}?format=j1"
-
-        embeds = self._create_embeds(ctx, url)
+        embeds = self._create_embeds(ctx, name)
         if not isinstance(embeds, list):
             await ctx.reply(embed=embeds)
             return
